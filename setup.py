7c9b9cdcaed6d5a22ec9382f722099460e9f3836
bilalpeerzade <121493640+bilalpeerzade@users.noreply.github.com>
2022-12-27 05:51:46 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup (
     name="formatSlackMessage",
-    version="1.0.0",
+    version="1.0.1",
     description="A project to format the slack messages in a structured way",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
@@ -12,7 +12,7 @@ setup (
     packages=['formatslackmessage'],
     package_dir={"":"src"},
     install_requires=[
-        "pandas >= 1.5.0"
+        "pandas >= 1.1.5"
     ],
     classifiers=[
         "Programming Language :: Python :: 3.6",
