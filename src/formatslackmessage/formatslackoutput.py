ba96e90c792a33348fd4d3ee83116e5e1670157b
bilalpeerzade <121493640+bilalpeerzade@users.noreply.github.com>
2022-12-27 20:34:53 -0800

    removing class and working directly with functions

--- src/formatslackmessage/formatslackoutput.py
+++ src/formatslackmessage/formatslackoutput.py
@@ -5,8 +5,8 @@ Created on Dec 26, 2022
 
 import pandas as pd
 
-class FormatSlackOutput:
-  def formatSlackMessage(self,df):
+
+def formatSlackMessage(self,df):
     df = df.applymap(str)
     max_lengths = df.apply(lambda x: (x.str.len().max() if x.str.len().max() > len(x.name) else len(x.name)) if x.dtype == 'object' else len(x.name))
     separator = '+'
@@ -28,15 +28,5 @@ class FormatSlackOutput:
         i+=1
       rows=rows+'\n'
     output = separator+header+separator+rows+separator
-    print(output)
     return output
     
-def main():
-    frmt = FormatSlackOutput()
-    
-    
-    data=[{"ID":1,"Name":"Alice","Age":25,"Address":{"Street":"123MainStreet","City":"NewYork","State":"NY"}},{"ID":2,"Name":"Bob","Age":30,"Address":{"Street":"456SecondStreet","City":"Chicago","State":"IL"}}]
-    df = pd.json_normalize(data)
-    frmt.formatSlackMessage(df)
-if __name__ == "__main__":
-    main()
